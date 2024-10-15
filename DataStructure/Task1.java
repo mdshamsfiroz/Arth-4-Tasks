22344601f7da2fb76ee8d41c5eabc70f94079b22
mdshamsfiroz <firozsaif01@gmail.com>
2024-10-15 15:38:23 +0530

    first commit:construction:

--- DataStructure/Task1.java
+++ DataStructure/Task1.java
@@ -1,4 +1,4 @@
-
+//1. Write a program to create a linked list, insert elements, and display the list. Implement basic operations like insert, delete, and reverse the list.
 class Node {
     int data;
     Node next;
