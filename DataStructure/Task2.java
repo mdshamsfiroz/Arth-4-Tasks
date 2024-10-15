370b2972367636dbf29af67ac32eeab40e8accc0
mdshamsfiroz <firozsaif01@gmail.com>
2024-10-15 15:52:51 +0530

    binarysearch

---
+++ DataStructure/Task2.java
@@ -0,0 +1,42 @@
+//Implement a binary search algorithm in any language, with an array of integers as input. The program should return the position of the searched element or state if it’s not present.
+public class Task2 {
+
+    // Method to perform binary search
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Check if the target is present at mid
+            if (arr[mid] == target) {
+                return mid;
+            }
+            // If target is greater, ignore the left half
+            else if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            // If target is smaller, ignore the right half
+            else {
+                right = mid - 1;
+            }
+        }
+
+        // Target is not present in the array
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 5;
+
+        int result = binarySearch(arr, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
